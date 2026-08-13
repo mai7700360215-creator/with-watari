f1e0463dba10a0cf1f318937f5be37dc5d014779
watari-daily <mkoak3793@outlook.jp>
2026-08-13 21:08:11 +0900

    game

--- game/game.js
+++ game/game.js
@@ -1,37 +1,22 @@
 let selectedDrink = null;
 
-
-// 飲み物を選ぶ
 function chooseDrink(drink) {
   selectedDrink = drink;
 
   const cup = document.getElementById("cup");
 
-  // 空のカップを表示
   cup.src = "images/cup.png";
   cup.style.display = "block";
 }
 
-
-// カップを押したら確認
 function pourDrink() {
-  if (!selectedDrink) {
-    return;
-  }
+  if (!selectedDrink) return;
 
   const cup = document.getElementById("cup");
 
   cup.src = "images/" + selectedDrink + ".png";
 }
 
-
-// カップ
 const cup = document.getElementById("cup");
 
-
-// PC
-cup.addEventListener("click", pourDrink);
-
-
-// スマホ
-cup.addEventListener("touchend", pourDrink);
+cup.onclick = pourDrink;
